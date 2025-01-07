0575796cf7362a2ec5e57614b6c41045e81913f6
RBB105 <Rba1pcrepairs@gmail.com>
2025-01-08 00:14:05 +1100

    WOWCub

--- fb.py
+++ fb.py
@@ -8,7 +8,7 @@ if sys.version_info[0] != 3:
     fb.py\n\t--------------------------------------''')
     sys.exit()
 
-PASSWORD_FILE = "passwords.txt"
+PASSWORD_FILE = "c:\Users\WIPED\Desktop\10MPASS.txt"
 MIN_PASSWORD_LENGTH = 6
 POST_URL = 'https://www.facebook.com/login.php'
 HEADERS = {
